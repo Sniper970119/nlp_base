d8d1041a973e5534dfc5a9c46bc9f7d52cab5a2d
Sniper970119 <359366783@qq.com>
2020-10-22 21:08:03 +0800

    translate data prepare

--- translation/run.py
+++ translation/run.py
@@ -18,3 +18,10 @@
           ┃ ┫ ┫   ┃ ┫ ┫
           ┗━┻━┛   ┗━┻━┛
 """
+
+import tensorflow as tf
+
+for gpu in tf.config.experimental.list_physical_devices('GPU'):
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
--- translation/utils.py
+++ translation/utils.py
@@ -18,3 +18,26 @@
           ┃ ┫ ┫   ┃ ┫ ┫
           ┗━┻━┛   ┗━┻━┛
 """
+
+import tensorflow as tf
+
+
+def add_BOS_and_EOS(sentence):
+    """
+    增加BOS EOS标记
+    :param sentence:
+    :return:
+    """
+    sentence = sentence.lower().strip()
+    sentence = '<BOS>' + sentence + '<EOS>'
+    return sentence
+
+
+def load_file(file_name, example_nub):
+    lines = open(file_name, encoding='utf-8').read().strip().split('\n')
+    datas = [[add_BOS_and_EOS(s) for s in l.split('\t')] for l in lines[:example_nub]]
+    return zip(*datas)
+
+
+if __name__ == '__main__':
+    load_file('./data/en-zh.csv', 100)
