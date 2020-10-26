c53ff11b4f3921cfef4084df7331de2a52855b84
Sniper970119 <359366783@qq.com>
2020-10-26 20:41:07 +0800

    update decoder.py

--- translation/models/decoder.py
+++ translation/models/decoder.py
@@ -18,3 +18,24 @@
           ┃ ┫ ┫   ┃ ┫ ┫
           ┗━┻━┛   ┗━┻━┛
 """
+
+import tensorflow as tf
+
+from translation.models.encoderLayer import EncoderLayer
+from translation.models.decoderLayer import DecoderLayer
+from translation.models.model_utils import *
+
+
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self, n_layers, d_model, n_heads, ddf, target_vocab_size, max_seq_len, dropout_rate=0.1):
+        super(Decoder, self).__init__()
+
+        self.d_model = d_model
+        self.n_layers = n_layers
+
+        self.embedding = tf.keras.layers.Embedding(target_vocab_size, d_model)
+        self.pos_embedding = positional_encoding(max_seq_len, d_model)
+
+        self.decode_layers = [DecoderLayer(d_model, n_heads, ddf, dropout_rate) for _ in range(n_layers)]
+
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
